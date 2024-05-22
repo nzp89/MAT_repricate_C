f19406058251fcf145d9007428c7950ccde07b00
nzp89 <92073106+nzp89@users.noreply.github.com>
2024-05-22 18:51:21 +0900

    Update mat.c
    これはこれでいいね版

--- mat.c
+++ mat.c
@@ -8,7 +8,6 @@
 #define TAU_M 5.0 // time constant [ms]
 #define R_RESIST 50.0 // membrane resistance [MOhm]
 #define L_NUM 2 // the number of threshold time constants
-#define OMEGA_REST 19.0 // resting value [mV]
 #define REFRACTORY 2.0 // absolute resractory period [ms]
 #define A_SCALE 0.1 // scale factor (0.1 -- 1.2)
 
@@ -18,7 +17,7 @@
 #define TAU_INH 3.0 // inh decay-time constant [ms]
 
 #define DT 0.001 // step size [ms]
-#define MAX_T 100.0 // max time [ms]
+#define MAX_T 1000.0 // max time [ms]
 
 double Sum1(double time, bool spike, double tau, double old_sum1){
     return exp(-DT / tau) * old_sum1 + spike;
@@ -32,6 +31,7 @@ double g_calculate(double time, double tau, double sum1, double sum2){
     return ((time + DT) * sum1 - sum2) / tau;
 }
 
+
 double i_current(double g_exc, double g_inh){
     return A_SCALE * I_EXC * g_exc + A_SCALE * I_INH * g_inh;
 }
@@ -40,44 +40,35 @@ double dvdt(double v_memb, double i_current){
     return ( -v_memb + R_RESIST * i_current) / TAU_M;
 }
 
-double H_t(double time, double *alpha, double *tau){
-    double sum = 0;
-    for (int i = 0; i < L_NUM; i++){
-        sum += alpha[i] * exp(-time / tau[i]); 
-    }
-    return sum;
-}
-
-double theta_t(double time){
-    double sum = 0;
-    
-    return OMEGA_REST;
-}
-
-
 int main(){
     double sum1_e, sum2_e, sum1_i, sum2_i;
     sum1_e = 0;
     sum2_e = 0;
     sum1_i = 0;
     sum2_i = 0;
-    double i_curr, g_exc, g_inh, frec;
+    double i_curr, g_exc, g_inh, frec, v_memb, threshold, b_volt, tau_theta, threshold_0;
+    v_memb = 0;
+    threshold = 0;
+    threshold_0 = -1;
+
+    b_volt = 1; // <=> omega
+    tau_theta = 5000; // <=> H tau
 
     frec = 6.88;
 
     int maxtime_count = MAX_T / DT;       
-    bool spike = 0;
 
-    FILE *i_current_file;
-    char *i_filename = "i_current.dat";
-    i_current_file = fopen(i_filename, "w");
-    
+    FILE *mat_all_file;
+    char *m_filename = "mat_all.dat";
+    mat_all_file = fopen(m_filename, "w");
+        
     uint32_t seed = (uint32_t)time(NULL);
     sfmt_t rng;
     sfmt_init_gen_rand(&rng, seed); 
 
 
     for (int time_count = 0; time_count < maxtime_count; time_count++){
+        double old_threshold = threshold;
         double time_ms = time_count * DT;
         double rand = sfmt_genrand_real2(&rng);
         bool spike = (rand < frec * DT);
@@ -96,11 +87,19 @@ int main(){
 
         i_curr = i_current(g_exc, g_inh);
 
-        fprintf(i_current_file, "%lf %lf\n", time_ms, i_curr);
+        v_memb += DT * dvdt(v_memb, i_curr);
+        threshold = exp(-DT / tau_theta) * old_threshold + (1.0 - exp(-DT / tau_theta)) * threshold_0;
+        spike = (v_memb >= threshold) ? 1: 0;
+        threshold = threshold + spike * b_volt;
+        if(spike){
+            fprintf(mat_all_file, "%lf %lf %lf %lf\n", time_ms, i_curr, v_memb + 10, threshold);
+        }
+
+        fprintf(mat_all_file, "%lf %lf %lf %lf\n", time_ms, i_curr, v_memb, threshold);
 
     }
 
-    fclose(i_current_file);
+    fclose(mat_all_file);
 
     return 0;
 }
